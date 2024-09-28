97ff6276c859b89121d453eb8388e4014ffa89c7
Rafaelkrc <rafaelkrc@hotmail.com>
2024-09-28 15:32:03 +0100

    created register user
    close #21

--- contact/urls.py
+++ contact/urls.py
@@ -5,9 +5,14 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/list/search/', views.search, name='search'),
+
+    # contact (CRUD)
     path('contact/detail/<int:contact_id>/', views.contactdetail, name='contact'),
     path('contact/update/<int:contact_id>/', views.update, name='update'),
     path('contact/delete/<int:contact_id>/', views.delete, name='delete'),
-    path('contact/list/search/', views.search, name='search'),
     path('contact/create/', views.create, name='create'),
+
+    # user
+    path('user/register/', views.register, name='register'),
 ]
