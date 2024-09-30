e1cd445f9b42378025c910f40517b3d2b55b7248
Rafaelkrc <rafaelkrc@hotmail.com>
2024-09-30 10:34:09 +0100

    created login and logout user
    close #25

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
@@ -154,6 +155,26 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário registrado com sucesso!')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     return render(request, 'contact/user_create.html', {'form': form})
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido!')
+    
+    return render(request, 'contact/login.html', {'form': form})
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
